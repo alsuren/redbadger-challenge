9e250d05a22b2467020cbdda7154654ef83fef49
David Laban <alsuren@gmail.com>
2019-09-18 16:09:23 +0100

    skeleton implementation with TODOS
    I might re-visit makeGrid, because it scales with the
    size of the arena, whereas I could keep a sparse map
    of scent positions, which would scale with the number
    of dead robots.

--- index.test.ts
+++ index.test.ts
@@ -1,23 +1,32 @@
-import { driveRobots } from ".";
+import {driveRobots, makeGrid} from '.';
 
 const SAMPLE_INPUT = `
 5 3
 1 1 E
 RFRFRFRF
+
 3 2 N
 FRRFLLFFRRFLL
+
 0 3 W
 LLFFFLFLFL
-`.trim()
+`.trim();
 
 const SAMPLE_OUTPUT = `
 1 1 E
 3 3 N LOST
 2 3 S
-`.trim()
+`.trim();
 
-describe.skip("driveRobots", () => {
-    it("returns what it is given", () => {
-        expect(driveRobots(SAMPLE_INPUT)).toEqual(SAMPLE_OUTPUT)
-    })
-})
+describe.skip('driveRobots', () => {
+  it('returns the expected output', () => {
+    expect(driveRobots(SAMPLE_INPUT)).toEqual(SAMPLE_OUTPUT);
+  });
+});
+
+describe('makeGrid', () => {
+  it('returns an array of the correct shape', () => {
+    expect(makeGrid('10 20').length).toEqual(10);
+    expect(makeGrid('10 20')[0].length).toEqual(20);
+  });
+});
--- index.ts
+++ index.ts
@@ -1,3 +1,51 @@
+type Position = {
+  x: number;
+  y: number;
+  bearing: 'N' | 'E' | 'S' | 'W';
+};
+
+type Instruction = 'L' | 'R' | 'F';
+
 export function driveRobots(input: string): string {
-    return input;
+  const [sizeLine, rest] = input.split('\n', 1);
+  let grid = makeGrid(sizeLine);
+
+  for (const script of rest.split('\n\n')) {
+    const [pos, instructions] = script.split('\n');
+    const startPosition = getStartPosition(pos);
+    const end = getEndPosition(grid, startPosition, instructions);
+    // TODO: record scent at end poistion if dead, and describe
+    // end position in output.
+  }
+  return input;
+}
+
+export function makeGrid(sizeLine: string): boolean[][] {
+  const [x, y] = sizeLine.split(' ').map((i) => parseInt(i, 10));
+  return new Array(x).fill(null).map(() => new Array<boolean>(y).fill(false));
+}
+
+export function getStartPosition(position: string): Position {
+  const [x, y, bearing] = position.split(' ');
+  return {x: parseInt(x, 10), y: parseInt(y, 10), bearing} as Position;
+}
+
+export function getEndPosition(
+  grid: boolean[][],
+  position: Position,
+  instructions: string,
+): Position {
+  for (const instruction of instructions) {
+    position = getNextPosition(grid, position, instruction as Instruction);
+  }
+  return position;
+}
+
+export function getNextPosition(
+  grid: boolean[][],
+  position: Position,
+  instruction: Instruction,
+): Position {
+  // TODO: write implementation of instructions
+  return position;
 }
