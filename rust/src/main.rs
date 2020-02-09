ccdf3303784a6f52e13b7c91c010b700744b8483
David Laban <alsuren@gmail.com>
2020-02-09 15:52:11 +0000

    going on an unwrap hunt

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::{Error, Result};
+use anyhow::{bail, Error, Result};
 use enum_display_derive::Display;
 use itertools::Itertools;
 use std::collections::HashSet;
@@ -23,9 +23,17 @@ impl TryFrom<String> for Grid {
 
     fn try_from(size_line: String) -> Result<Self, Self::Error> {
         let mut split = size_line.split(' ');
-        let x = split.next().map(|i| i.parse::<i32>().unwrap()).unwrap();
-        let y = split.next().map(|i| i.parse::<i32>().unwrap()).unwrap();
-        assert!(split.next().is_none(), "grid line has too many fields");
+        let x = split
+            .next()
+            .ok_or_else(|| Error::msg("missing x coordinate"))?
+            .parse::<i32>()?;
+        let y = split
+            .next()
+            .ok_or_else(|| Error::msg("missing y coordinate"))?
+            .parse::<i32>()?;
+        if let Some(_) = split.next() {
+            bail!("grid line has too many fields");
+        }
         Ok(Grid {
             x_max: x,
             y_max: y,
@@ -49,10 +57,21 @@ impl TryFrom<String> for Position {
 
     fn try_from(position_line: String) -> Result<Self, Self::Error> {
         let mut split = position_line.split(' ');
-        let x = split.next().map(|i| i.parse::<i32>().unwrap()).unwrap();
-        let y = split.next().map(|i| i.parse::<i32>().unwrap()).unwrap();
-        let bearing = split.next().map(|i| i.try_into().unwrap()).unwrap();
-        assert!(split.next().is_none(), "start position has too many fields");
+        let x = split
+            .next()
+            .ok_or_else(|| Error::msg("missing x coordinate"))?
+            .parse::<i32>()?;
+        let y = split
+            .next()
+            .ok_or_else(|| Error::msg("missing y coordinate"))?
+            .parse::<i32>()?;
+        let bearing = split
+            .next()
+            .ok_or_else(|| Error::msg("missing bearing"))?
+            .try_into()?;
+        if let Some(_) = split.next() {
+            bail!("grid line has too many fields");
+        }
         Ok(Position { x, y, bearing })
     }
 }
@@ -102,7 +121,7 @@ impl TryFrom<&str> for Bearing {
 }
 
 impl Bearing {
-    fn rotate(self, rotation: Rotation) -> Bearing {
+    fn rotate(self, rotation: &Rotation) -> Bearing {
         use Bearing::*;
         use Rotation::*;
 
@@ -180,7 +199,7 @@ fn go_forwards(grid: &Grid, position: Position) -> Position {
     }
 }
 
-fn get_next_position(grid: &Grid, position: Position, instruction: Instruction) -> Position {
+fn get_next_position(grid: &Grid, position: Position, instruction: &Instruction) -> Position {
     // If we're already off the edge of the board then skip all instructions
     if is_out_of_bounds(grid, &position) {
         return position;
@@ -195,11 +214,10 @@ fn get_next_position(grid: &Grid, position: Position, instruction: Instruction) 
     }
 }
 
-fn get_end_position(grid: &Grid, position: Position, instructions: &str) -> Position {
+fn get_end_position(grid: &Grid, position: Position, instructions: &[Instruction]) -> Position {
     let mut current = position;
-    for instruction in instructions.chars() {
-        let parsed_instruction = instruction.try_into().unwrap();
-        current = get_next_position(grid, current, parsed_instruction);
+    for instruction in instructions {
+        current = get_next_position(grid, current, instruction);
     }
     current
 }
@@ -266,8 +284,12 @@ fn drive_robots(
 
     let output = lines.tuples().map(
         move |(position_line, instruction_line): (Result<String>, Result<String>)| {
-            let start = position_line?.try_into().unwrap();
-            let end = get_end_position(&grid, start, &instruction_line?);
+            let start = position_line?.try_into()?;
+            let instructions = instruction_line?
+                .chars()
+                .map(|c| c.try_into())
+                .collect::<Result<Vec<_>>>()?;
+            let end = get_end_position(&grid, start, &instructions);
             if is_out_of_bounds(&grid, &end) {
                 // robots stay where they are as soon as they fall off the world,
                 // so if we back the robot up then we will have the position where
@@ -294,7 +316,7 @@ fn main() -> anyhow::Result<()> {
 
     drive_robots(lines)
         .flatten()
-        .for_each(|result| println!("{}", result.unwrap()));
+        .try_for_each(|result| Ok::<_, Error>(println!("{}", result?)))?;
 
     Ok(())
 }
@@ -307,20 +329,19 @@ mod tests {
         input.lines().map(|l| Ok(l.trim().to_owned()))
     }
 
-    fn join(input: impl Iterator<Item = Result<String>>) -> String {
-        input
-            .collect::<Result<Vec<String>>>()
-            .unwrap()
+    fn join(input: impl Iterator<Item = Result<String>>) -> Result<String> {
+        Ok(input
+            .collect::<Result<Vec<String>>>()?
             .join("\n")
-            .to_owned()
+            .to_owned())
     }
 
-    fn format(input: &str) -> String {
+    fn format(input: &str) -> Result<String> {
         join(split(input).filter(is_interesting))
     }
 
     #[test]
-    fn example_input_produces_example_output() {
+    fn example_input_produces_example_output() -> Result<()> {
         let input = r#"
         5 3
         1 1 E
@@ -330,7 +351,7 @@ mod tests {
         0 3 W
         LLFFFLFLFL
         "#;
-        let output = join(drive_robots(split(input)).flatten());
+        let output = join(drive_robots(split(input)).flatten())?;
 
         let expected_output = format(
             r#"
@@ -338,17 +359,20 @@ mod tests {
             3 3 N LOST
             2 3 S
             "#,
-        );
+        )?;
         assert_eq!(output, expected_output);
+        Ok(())
     }
-    #[test]
-    fn empty_input_produces_error() {
-        let input = r#""#;
-        let output = drive_robots(split(input)).flatten().next();
 
-        assert_eq!(
-            output.unwrap().unwrap_err().to_string(),
-            "input must not be empty"
-        );
+    #[test]
+    fn empty_input_produces_error() -> Result<()> {
+        let input = r#""#;
+        let output = drive_robots(split(input))
+            .flatten()
+            .next()
+            .ok_or_else(|| Error::msg("should output something"))?;
+
+        assert_eq!(output.unwrap_err().to_string(), "input must not be empty");
+        Ok(())
     }
 }
